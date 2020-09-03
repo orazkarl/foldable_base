3d6caa95e6e28db58636da9e7109be924eef5406
orazkarl <43617375+orazkarl@users.noreply.github.com>
2020-09-03 11:56:39 +0600

    Total stats done

--- analytics_app/views.py
+++ analytics_app/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from appbase.models import Object, Contract
+from appbase.models import Object, Contract, RequestForMaterial
 from material_app.models import Material, ReleaseMaterial, ReleaseMaterialItem
 from .filters import MaterialFilter
 from django.db.models import Count, Sum
-
+import datetime
+import pytz
 
 @method_decorator(login_required(login_url='/accounts/login/'), name='dispatch')
 class AnalyticsView(generic.ListView):
@@ -17,15 +18,14 @@ class AnalyticsView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         contracts = Contract.objects.filter(contstruct_object__slug=self.kwargs['slug'])
-        queryset = self.get_queryset().filter(is_delivery=True, invoice__is_done=True, invoice__request_mat__contract__contstruct_object__slug=self.kwargs['slug'])
+        queryset = self.get_queryset().filter(is_delivery=True, invoice__is_done=True,
+                                              invoice__request_mat__contract__contstruct_object__slug=self.kwargs[
+                                                  'slug'])
         material_filter = MaterialFilter(self.request.GET, queryset=queryset)
         context['total_sum_price'] = material_filter.qs.aggregate(Sum('sum_price'))['sum_price__sum']
         context['filter'] = material_filter
 
-
         return context
-
-
 
     def get(self, request, *args, **kwargs):
         object_slug = self.kwargs['slug']
@@ -36,13 +36,58 @@ class AnalyticsView(generic.ListView):
         }
         return super().get(request, *args, **kwargs)
 
+
 @method_decorator(login_required(login_url='/accounts/login/'), name='dispatch')
 class TotalStats(generic.TemplateView):
     template_name = 'analytics/total_stats.html'
 
     def get(self, request, *args, **kwargs):
         object_slug = self.kwargs['slug']
+        contracts = Contract.objects.filter(contstruct_object__slug=object_slug)
+        request_mats = RequestForMaterial.objects.filter(contract__contstruct_object__slug=object_slug)
+        materials = Material.objects.filter(invoice__request_mat__contract__contstruct_object__slug=object_slug)
+        total_sum_price = materials.aggregate(Sum('sum_price'))['sum_price__sum']
+
+        start_date_default = datetime.datetime(2020, 8, 1, tzinfo=pytz.UTC)
+        end_date_default = datetime.datetime.today().date()
+        in_work = contracts.filter(status='1')
+        finished = contracts.filter(status='2')
+        not_finished = contracts.filter(status='3')
+        check = contracts.filter(status='4')
+        is_done_request_mat = request_mats.filter(is_done=True)
+        if 'start_date' in request.GET:
+            start_date_default = request.GET['start_date'].replace('-', '/')
+            end_date_default = request.GET['end_date'].replace('-', '/')
+            start_date_default = datetime.datetime.strptime(start_date_default, '%Y/%m/%d')
+            end_date_default = datetime.datetime.strptime(end_date_default, '%Y/%m/%d')
+            contracts = contracts.filter(created_at__gte=start_date_default,
+                                         created_at__lte=end_date_default)
+            request_mats = request_mats.filter(created_at__gte=start_date_default,
+                                         created_at__lte=end_date_default)
+            materials = materials.filter(created_at__gte=start_date_default,
+                                         created_at__lte=end_date_default)
+
+            in_work = contracts.filter(status='1')
+            finished = contracts.filter(status='2')
+            not_finished = contracts.filter(status='3')
+            check = contracts.filter(status='4')
+            is_done_request_mat = request_mats.filter(is_done=True)
+            total_sum_price = materials.aggregate(Sum('sum_price'))['sum_price__sum']
+
+
         self.extra_context = {
             'object': Object.objects.get(slug=object_slug),
+            'contracts': contracts,
+            'request_mats': request_mats,
+            'materials': materials,
+            'total_sum_price': total_sum_price,
+            'start_date_default': start_date_default,
+            'end_date_default': end_date_default,
+            'in_work': in_work,
+            'finished': finished,
+            'not_finished': not_finished,
+            'check': check,
+            'is_done_request_mat': is_done_request_mat,
+
         }
         return super().get(request, *args, **kwargs)
